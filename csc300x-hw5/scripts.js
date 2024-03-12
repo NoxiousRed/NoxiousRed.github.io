ac0145a638e83bf175eb25527c4c12b8c2740e39
Craig Smith <craigshmitty6@gmail.com>
2024-03-12 12:13:37 -0400

    set up page, preparing repo cards

---
+++ csc300x-hw5/scripts.js
@@ -0,0 +1,75 @@
+(function () {
+
+    const GITHUB_API_BASEURL = 'https://api.github.com/users/';
+    const GITHUB_BASE_USERNAME = 'noxiousred'
+    window.addEventListener('load', init);
+
+    function init() {
+        console.log("Window Loaded!")
+        getUserRepos(GITHUB_BASE_USERNAME)
+        id('get-username').addEventListener('submit', function (e) {
+            // if we've gotten in here, all HTML5 validation checks have passed
+            e.preventDefault();
+            submitForm();
+        });
+    }
+
+    function submitForm() {
+        let gitHubUsername = document.getElementById('github-username').value;// pass in the entered github-username
+        getUserRepos(gitHubUsername)
+    }
+
+
+
+    function getUserRepos(gitHubUsername) {
+        //want to replace gitHubUsername with the input user sends in
+        let url = GITHUB_API_BASEURL + gitHubUsername + '/repos?sort=created'
+
+        //chaining thens allows you to access whatever data is returned in a then to following thens.
+        fetch(url)
+            .then(checkStatus)
+            .then((repoData) => {
+                console.log(repoData);
+                let div = id('card-container');
+
+                for (const item of repoData) {
+                    let repoCard = document.createElement('repo-card')
+
+                    let repoName = item['name'];
+                    //replace this with the necessary href link VVV
+                    repoName.innerHTML = 'Name: ' + name;
+                    div.appendChild(repoName);
+
+                    let repoDate = document.createElement('p');
+                    const date = item['created_at'];
+                    repoDate.innerHTML = 'Created: ' + date;
+                    div.appendChild(repoDate);
+
+                    let upateDate = document.createElement('p');
+                    const updated = item['updated_at'];
+                    upateDate.innerHTML = 'Updated: ' + updated;
+                    div.appendChild(upateDate);
+
+                    let rule = document.createElement('hr');
+                    div.appendChild(rule);
+
+                    div.appendChild(repoCard);
+                }
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+    }
+
+    //helper functions
+    function id(idName) {
+        return document.getElementById(idName);
+    }
+    function checkStatus(response) {
+        if (!response.ok) {
+            throw Error('Error in request: ' + response.statusText);
+        }
+        return response.json();
+    }
+
+})();
